4992ca5b1425c7ceeb1a60037366f46cb1b12968
mvotho <jackfaulknerbis@hotmail.co.uk>
2022-08-16 15:49:21 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './main.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-sky-500/100">
+      
+    </div>
+  )
+}
+
+export default App
